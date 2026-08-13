5fca217f68c39c96384155d6291adaee87e479d6
James Sharp <jamesrosssharp@gmail.com>
2026-08-13 21:29:28 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+import numpy as np
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name="mag_parser",
+    ext_modules=cythonize("src/mag_parser.pyx", compiler_directives={'language_level': "3"}),
+    include_dirs=[np.get_include(), "src"]
+)
